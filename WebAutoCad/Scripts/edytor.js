20b3556da85fc1d3a6df73eaec2f6b1bb8582287
rafal <satrnn@gmail.com>
2017-04-24 19:42:15 +0200

    lekka zmiana koncepcji

--- WebAutoCad/Scripts/edytor.js
+++ WebAutoCad/Scripts/edytor.js
@@ -1,14 +1,22 @@
-﻿var svg = $("svg");
+﻿var mode = 0; //select
+$("#btn-select").click(function(){
+    mode = 0;
+});
+$("#btn-line").click(function(){
+    mode = 1;
+});
+
+
+var svg = $("svg");
 var layer = $("#layer1");
 
 var dragElemnt = null;
-var startPoint = null;
 
 var idIterator = 0;
-
+/*
 function createPoint(x, y)
 {
-    var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");//.createElement("");
+    var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     rect.setAttribute("x", x - 5);
     rect.setAttribute("y", y - 5);
     rect.setAttribute("class", "point");
@@ -26,24 +34,19 @@ function createPoint(x, y)
 
     return rect;
 
-}
+}*/
 
-function addPoint(x, y)
+function selectLine(node)
 {
-    var rect = createPoint(x, y);
-    layer[0].appendChild(rect);
-
-    return rect;
+    $(".line_options").show();
 }
 
 function addLine(x, y)
 {
     var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    var startPoint = createPoint(x, y);
-    var endPoint = createPoint(x, y);
     var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
     
-    line.setAttribute("class", "line");
+    line.setAttribute("class", "line creating");
     line.setAttribute("x1", x);
     line.setAttribute("y1", y);
     line.setAttribute("x2", x);
@@ -52,46 +55,57 @@ function addLine(x, y)
     group.setAttribute("id", "line"+idIterator++);
 
     group.appendChild(line);
-    group.appendChild(startPoint);
-    group.appendChild(endPoint);
-    
 
     layer[0].appendChild(group);
+
+    line.addEventListener("click", function(e){
+        e.stopPropagation();
+        selectLine(this);
+    });
+
     return {
         group: group,
-        startPoint: startPoint,
-        endPoint: endPoint,
         line: line,
     };
 }
 
 svg[0].addEventListener("mousedown", function(e)
 {
-    var line = addLine(e.offsetX, e.offsetY);
-    dragElemnt = line.endPoint;
+    if(mode == 1)
+    {
+        var line = addLine(e.offsetX, e.offsetY);
+        dragElemnt = line;
+    }
 });
 
 svg[0].addEventListener("mouseup", function(e){
     e.stopPropagation();
+    if(mode == 1)
+    {
+        var x1 = dragElemnt.line.getAttribute("x1");
+        var y1 = dragElemnt.line.getAttribute("y1");
+        
+        var x2 = dragElemnt.line.getAttribute("x2");
+        var y2 = dragElemnt.line.getAttribute("y2");
+
+        if(x1 == x2 && y1 == y2){
+            dragElemnt.group.parentNode.removeChild(dragElemnt.group);
+        }
+        else
+        {
+            dragElemnt.line.setAttribute("class", "line");
+            selectLine(dragElemnt);
+        }
+    }
+   
     dragElemnt = null;
 });
 
 svg[0].addEventListener("mousemove", function(e){
     
     if(dragElemnt != null){
-        dragElemnt.setAttribute("x", e.offsetX - 5);
-        dragElemnt.setAttribute("y", e.offsetY - 5);
-
-        var line = dragElemnt.parentNode.querySelectorAll("line")[0];
-        var points = dragElemnt.parentNode.querySelectorAll("rect");
-        var x1 = parseFloat(points[0].getAttribute("x")) + 5;
-        var y1 = parseFloat(points[0].getAttribute("y")) + 5;
-        var x2 = parseFloat(points[1].getAttribute("x")) + 5;
-        var y2 = parseFloat(points[1].getAttribute("y")) + 5;
-        line.setAttribute("x1",x1);
-        line.setAttribute("x2",x2);
-        line.setAttribute("y1",y1);
-        line.setAttribute("y2",y2);
+        dragElemnt.line.setAttribute("x2", e.offsetX);
+        dragElemnt.line.setAttribute("y2", e.offsetY);
     }
     else
     {
