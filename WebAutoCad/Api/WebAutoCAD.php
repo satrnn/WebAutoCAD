604ae76d3d131d0c60732ab307383c2858296b9d
Voltex <voltex15@gmail.com>
2017-05-05 20:31:14 +0200

    Dodanie ajaxa do strony

--- WebAutoCad/Api/WebAutoCAD.php
+++ WebAutoCad/Api/WebAutoCAD.php
@@ -4,12 +4,21 @@
 
         $result = array();
 
+		var_dump($_GET);
         echo json_encode($result);
     }
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		
         // Zapis danych
-        $result = (object) array('success' => true);
+        $result = array('success' => true);
+		
+		$dane= $_POST;
+		
+		$data = json_decode( file_get_contents('php://input') );
 
+		$result['msg'] = "Udalo sie zapisac!";
+		$result['odpowiedz'] = var_dump( $_POST)
         echo json_encode($result);
+		
     }
 ?>
---
+++ WebAutoCad/Scripts/app/saveread.js
@@ -0,0 +1,36 @@
+﻿$("#btn-save").click(function(event){
+	//przygotowanie danych do zapisu
+    var dane = [];
+	for (var i = 0; i<lineManager.lines.length; i++)
+	{
+		var line = lineManager.lines[i];
+		
+		var dana = {
+		 x1: line.x1,
+		 x2: line.x2,
+		 y1: line.y1,
+		 y2: line.y2,
+		 state: line.state,
+		};
+		dane.push(dana);
+	}
+	lineManager.lines;
+	
+	
+	
+	$.ajax({
+		type: "POST",
+		url: "api/WebAutoCAD.php",
+		dataType: 'json',
+		data: { "v": dane },
+		success: function(data)
+		{
+			alert("Sukces");
+		},
+		error: function(x, y, z)
+		{
+			alert("Nie udalo sie!")
+		},
+	})
+	
+})
